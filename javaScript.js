b0f9620ae62f4447a92b0c249a78f876e2fd9bf7
Iuli4 <yulya.popa.962005@gmail.com>
2022-10-31 20:50:49 +0200

    dasda

--- javaScript.js
+++ javaScript.js
@@ -1,6 +1,83 @@
-let text="salut";
-let number=100;
+//let text="salut";
+//let number=100;
 
-console.log(text)
+//console.log(text)
 //alert(text + number)
-prompt(text)
+//prompt(text)
+
+//const a = 2;
+//const b = 3;
+//if (2=="2") {
+  //  console.log(true);
+//} else {
+ //   console.log(false);
+//}
+
+//if (a<=10) {
+  //  console.log("Правильно");
+//}
+  //  else if (a==="2"){
+      //  console.log("Равно");
+  // } else {
+   // console.log("Неправильно");
+    //}
+// const c =10;
+// const e =20;
+//if (c === 10 && e === 20) {
+  //  console.log("Они равны");
+//} else {
+  //  console.log("Не равны");
+//}
+//  if (c > 19 || e > 10){
+//     console.log("Одна правильная");
+//  } else{
+//     console.log("Не одна не правильная");
+//  }
+
+// let fruit = "orange";
+// switch (fruit) {
+//     case "orange":
+//         console.log("Orange $ 0.59");
+//         break;
+//     case "apple":
+//         console.log("Apple $ 1.59");
+//         break;
+//     case "banana":
+//             console.log("Banana $ 2.59");
+//             break;   
+
+//     default:
+//         break;
+// }
+//  let i=0;
+// while (i <= 10){
+//     console.log(i);
+//     i +=1;
+// }
+// do {
+//     console.log(i);
+//     i +=1;
+// } while (i<=5);
+// for (let i= 0; i < 5; i++) {
+//     console.log(i);
+// }
+    // do {
+    //     console.log(i);
+    //     i +=1;
+    // } while (i<=5);}
+ const first_number= prompt("First number");
+ const operator = prompt("mathematical operator");
+ const second_number = prompt ("Second number");
+ Number()
+ if (operator === "+"){
+    console.log(Number(first_number) + Number(second_number));
+ } else if (operator === "-"){
+    console.log(Number(first_number) - Number(second_number));
+ } else if (operator === "*"){
+    console.log(Number(first_number) * Number(second_number));
+} else if (operator === "/"){
+    console.log(Number(first_number) / Number(second_number));
+} else if (operator === "**"){
+    console.log(Number(first_number) ** Number(second_number));
+}
+
